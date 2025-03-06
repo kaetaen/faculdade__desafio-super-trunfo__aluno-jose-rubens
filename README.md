9bceadaad7cf136250c6b0339c2b4e77302d823b
kaetaen <kaetaen@tutanota.com>
2025-03-06 02:18:43 -0300

    feat: conclusão desafio supertrunfo (impl. logica do jogo):  nivel mestre

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Aluno: José Rubens dos Santos
 
+Jogo de Trunfo na modalidade Paises
+
 ## Dependencias
 
 - Compilador gcc (clang, gcc, etc)
@@ -25,8 +27,20 @@ gcc supertrunfo.c -o supertrunfo
 ## Como jogar
 
 Todas as regras do trunfo são aplicaveis. 
-Basta preencher duas cartas (rivais entre si), e escolher qual dos atributos você deseja comparar.
-O atributo com maior valor vence o jogo. 
+Basta preencher duas cartas que representam paises, as cartas são rivais entre si. Após o preenchimento das cartas escolha quais atributos dos paises você quer comparar.
+
+Abaixo segue os códigos correspondentes a cada atributo.
+Por meio deles você escolhe quais atributos quer comparar.
+
+| CODIGO | ATRIBUTO                     |
+|--------|------------------------------|
+| 1      | POPULAÇÃO                    |
+| 2      | ÁREA                         |
+| 3      | PIB                          |
+| 4      | NÚMERO DE PONTOS TURISTICOS  |
+| 5      | DENSIDADE POPULACIONAL       |
+
+A carta cuja soma total dos valores dos atributos for maior, vence o jogo.
 
 OBS.: Com exceção a densidade populacional, onde quem vence é quem possui o menor valor.
 
--- supertrunfo.c
+++ supertrunfo.c
@@ -25,9 +25,17 @@
         float densidade_populacional_carta02;
 
         // Variavéis de uso geral do jogo (menu, controle e resultado)
-        int menu_opcao_selecionada = 0;
-        char nome_atributo_selecionado[200];
-        int carta_vencedora;
+        int menu_atributo_selecionado_01 = 0;
+        int menu_atributo_selecionado_02 = 0;
+
+        char nome_atributo_selecionado_01[200];
+        char nome_atributo_selecionado_02[200];
+        
+        int carta_vencedora_primeira_rodada;
+        int carta_vencedora_segunda_rodada;
+
+        double soma_atributos_resultado_final_carta01 = 0.0;
+        double soma_atributos_resultado_final_carta02 = 0.0;
 
         // ENTRADA DE DADOS CARTA 001
         printf("Insira os dados da CARTA 01 \n\n");
@@ -45,7 +53,7 @@
         scanf("%f", &PIB_carta01);
         
         printf("Insira o numero de pontos turisticos: ");
-        scanf("%i", &numero_pontos_turisticos_carta01);
+        scanf("%d", &numero_pontos_turisticos_carta01);
         
         // ENTRADA DE DADOS CARTA 002
         printf("\n\nInsira os dados da CARTA 02 \n\n");
@@ -63,7 +71,7 @@
         scanf("%f", &PIB_carta02);
         
         printf("Insira o numero de pontos turisticos: ");
-        scanf("%i", &numero_pontos_turisticos_carta02);
+        scanf("%d", &numero_pontos_turisticos_carta02);
         
         // Calculo densidade populacional 
         densidade_populacional_carta01 = populacao_carta01 / area_carta01;
@@ -78,90 +86,171 @@
         printf("4 - NÚMERO DE PONTOS TÚRISTICOS\n");
         printf("5 - DENSIDADE POPULACIONAL\n");
         printf("-----------------------------------\n");
-        printf("Insira o código de uma propriedade para fazer a comparação: ");
-        scanf("%d", &menu_opcao_selecionada);
+        
+        printf("Escolha o primeiro atributo: ");
+        scanf("%d", &menu_atributo_selecionado_01);
+
+        printf("Escolha o segundo atributo: ");
+        scanf("%d", &menu_atributo_selecionado_02);
+        
         printf("-----------------------------------\n");
-
-        if (menu_opcao_selecionada < 1 || menu_opcao_selecionada > 5) {
-            printf("Erro: Opção inválida!\n");
-            return 1;
+        
+        if (menu_atributo_selecionado_01 == menu_atributo_selecionado_02) {
+            printf("ERRO: Você não pode selecionar dois atributos iguais!");
         }
-        printf("menu opção selecionada: ");
 
-        // Atribuição de nome do atributo e carta vencedora.
-        switch (menu_opcao_selecionada) {
+        // Atribuição de nome do atributo e carta vencedora da primeira rodada e calculo de soma final.
+        switch (menu_atributo_selecionado_01) {
             case 1:
-                strcpy(nome_atributo_selecionado, "População"); 
+                strcpy(nome_atributo_selecionado_01, "População"); 
                 
                 if (populacao_carta01 > populacao_carta02) {
-                    carta_vencedora = CARTA_01;
+                    carta_vencedora_primeira_rodada = CARTA_01;
                     
                 } else if (populacao_carta01 < populacao_carta02) {
-                    carta_vencedora = CARTA_02;
-                    
-                } else if (populacao_carta01 == populacao_carta02) {
-                    carta_vencedora = EMPATE;
+                    carta_vencedora_primeira_rodada = CARTA_02;    
                 }
+
+                soma_atributos_resultado_final_carta01 += populacao_carta01;  
+                soma_atributos_resultado_final_carta02 += populacao_carta02;
             break;
                 
             case 2:
-                strcpy(nome_atributo_selecionado, "Área"); 
+                strcpy(nome_atributo_selecionado_01, "Área"); 
                 
                 if (area_carta01 > area_carta02) {
-                    carta_vencedora = CARTA_01;
+                    carta_vencedora_primeira_rodada = CARTA_01;
                     
                 } else if (area_carta01 < area_carta02) {
-                    carta_vencedora = CARTA_02;
-                    
-                } else if (area_carta01 == area_carta02) {
-                    carta_vencedora = EMPATE;
+                    carta_vencedora_primeira_rodada = CARTA_02;   
                 }
+
+                soma_atributos_resultado_final_carta01 += area_carta01;  
+                soma_atributos_resultado_final_carta02 += area_carta02;
             break;
                 
             case 3:
-                strcpy(nome_atributo_selecionado, "PIB"); 
+                strcpy(nome_atributo_selecionado_01, "PIB"); 
                 
                 if (PIB_carta01 > PIB_carta02) {
-                    carta_vencedora = CARTA_01;
+                    carta_vencedora_primeira_rodada = CARTA_01;
                     
                 } else if (PIB_carta01 < PIB_carta02) {
-                    carta_vencedora = CARTA_02;
-                    
-                } else if (PIB_carta01 == PIB_carta02) {
-                    carta_vencedora = EMPATE;
+                    carta_vencedora_primeira_rodada = CARTA_02;   
                 }
+
+                soma_atributos_resultado_final_carta01 += PIB_carta01;  
+                soma_atributos_resultado_final_carta02 += PIB_carta02;
             break;
                 
             case 4:
-                strcpy(nome_atributo_selecionado, "Número de Pontos Turisticos"); 
+                strcpy(nome_atributo_selecionado_01, "Número de Pontos Turisticos"); 
                 
                 if (numero_pontos_turisticos_carta01 > numero_pontos_turisticos_carta02) {
-                    carta_vencedora = CARTA_01;
+                    carta_vencedora_primeira_rodada = CARTA_01;
                     
                 } else if (numero_pontos_turisticos_carta01 < numero_pontos_turisticos_carta02) {
-                    carta_vencedora = CARTA_02;
-                    
-                } else if (numero_pontos_turisticos_carta01 == numero_pontos_turisticos_carta02) {
-                    carta_vencedora = EMPATE;
+                    carta_vencedora_primeira_rodada = CARTA_02;    
                 }
+
+                soma_atributos_resultado_final_carta01 += numero_pontos_turisticos_carta01;  
+                soma_atributos_resultado_final_carta02 += numero_pontos_turisticos_carta02;
             break;
                 
             case 5:
-                strcpy(nome_atributo_selecionado, "Densidade Populacional"); 
+                strcpy(nome_atributo_selecionado_01, "Densidade Populacional"); 
                 
                 if (densidade_populacional_carta01 < densidade_populacional_carta02) {
-                    carta_vencedora = CARTA_01;
+                    carta_vencedora_primeira_rodada = CARTA_01;
 
                 } else if (densidade_populacional_carta01 > densidade_populacional_carta02) {
-                    carta_vencedora = CARTA_02;
-
-                } else if (densidade_populacional_carta01 == densidade_populacional_carta02) {
-                    carta_vencedora = EMPATE;
+                    carta_vencedora_primeira_rodada = CARTA_02;
                 }
+
+                soma_atributos_resultado_final_carta01 += densidade_populacional_carta01;  
+                soma_atributos_resultado_final_carta02 += densidade_populacional_carta02;
             break;
         
             default:
-                printf("A opção selecionada não existe!\n");
+                printf("A opção selecionada não existe! Encerrando programa... \n");
+                return 0;
+            break;
+        }
+
+        // Atribuição de nome do atributo e carta vencedora da primeira rodada e calculo de soma final.
+        switch (menu_atributo_selecionado_02) {
+            case 1:
+                strcpy(nome_atributo_selecionado_02, "População"); 
+                
+                if (populacao_carta01 > populacao_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_01;
+                    
+                } else if (populacao_carta01 < populacao_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_02;   
+                }
+                soma_atributos_resultado_final_carta01 += populacao_carta01;  
+                soma_atributos_resultado_final_carta02 += populacao_carta02;  
+            break;
+                
+            case 2:
+                strcpy(nome_atributo_selecionado_02, "Área"); 
+                
+                if (area_carta01 > area_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_01;
+                    
+                } else if (area_carta01 < area_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_02;   
+                }
+
+                soma_atributos_resultado_final_carta01 += area_carta01;  
+                soma_atributos_resultado_final_carta02 += area_carta02;
+            break;
+                
+            case 3:
+                strcpy(nome_atributo_selecionado_02, "PIB"); 
+                
+                if (PIB_carta01 > PIB_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_01;
+                    
+                } else if (PIB_carta01 < PIB_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_02;
+                }
+
+                soma_atributos_resultado_final_carta01 += PIB_carta01;  
+                soma_atributos_resultado_final_carta02 += PIB_carta02;
+            break;
+                
+            case 4:
+                strcpy(nome_atributo_selecionado_02, "Número de Pontos Turisticos"); 
+                
+                if (numero_pontos_turisticos_carta01 > numero_pontos_turisticos_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_01;
+                    
+                } else if (numero_pontos_turisticos_carta01 < numero_pontos_turisticos_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_02;   
+                }
+
+                soma_atributos_resultado_final_carta01 += numero_pontos_turisticos_carta01;  
+                soma_atributos_resultado_final_carta02 += numero_pontos_turisticos_carta02;
+            break;
+                
+            case 5:
+                strcpy(nome_atributo_selecionado_02, "Densidade Populacional"); 
+                
+                if (densidade_populacional_carta01 < densidade_populacional_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_01;
+
+                } else if (densidade_populacional_carta01 > densidade_populacional_carta02) {
+                    carta_vencedora_segunda_rodada = CARTA_02;
+                }
+
+                soma_atributos_resultado_final_carta01 += densidade_populacional_carta01;  
+                soma_atributos_resultado_final_carta02 += densidade_populacional_carta02;
+            break;
+        
+            default:
+                printf("A opção selecionada não existe! Encerrando programa... \n");
+                return 0;
             break;
         }
 
@@ -173,43 +262,78 @@
         
         printf("Nome dos países: %s (CARTA 1) e %s (CARTA 2) \n", nome_pais_carta01, nome_pais_carta02);
         
-        switch (menu_opcao_selecionada) {
+        switch (menu_atributo_selecionado_01) {
             case 1:
-                printf("Valor do atributo %s para carta 1: %li \n", nome_atributo_selecionado, populacao_carta01);
-                printf("Valor do atributo %s para carta 2: %li \n", nome_atributo_selecionado, populacao_carta02);
+                printf("1° Rodada: Valor do atributo %s para carta 1: %li \n", nome_atributo_selecionado_01, populacao_carta01);
+                printf("1° Rodada: Valor do atributo %s para carta 2: %li \n", nome_atributo_selecionado_01, populacao_carta02);
             break;
             
             case 2:
-                printf("Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado, area_carta01);
-                printf("Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado, area_carta02);
+                printf("1° Rodada: Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado_01, area_carta01);
+                printf("1° Rodada: Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado_01, area_carta02);
             break;
                         
             case 3:
-                printf("Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado, PIB_carta01);
-                printf("Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado, PIB_carta01);
+                printf("1° Rodada: Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado_01, PIB_carta01);
+                printf("1° Rodada: Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado_01, PIB_carta01);
             break;
         
             case 4:
-                printf("Valor do atributo %s para carta 1: %d \n", nome_atributo_selecionado, numero_pontos_turisticos_carta01);
-                printf("Valor do atributo %s para carta 2: %d \n", nome_atributo_selecionado, numero_pontos_turisticos_carta02);
+                printf("1° Rodada: Valor do atributo %s para carta 1: %d \n", nome_atributo_selecionado_01, numero_pontos_turisticos_carta01);
+                printf("1° Rodada: Valor do atributo %s para carta 2: %d \n", nome_atributo_selecionado_01, numero_pontos_turisticos_carta02);
             break;
 
             case 5:
-                printf("Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado, densidade_populacional_carta01);
-                printf("Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado, densidade_populacional_carta02);
+                printf("1° Rodada: Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado_01, densidade_populacional_carta01);
+                printf("1° Rodada: Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado_01, densidade_populacional_carta02);
             break;
 
             default:
                 break;
         }
+
+        switch (menu_atributo_selecionado_02) {
+            case 1:
+                printf("2° Rodada: Valor do atributo %s para carta 1: %li \n", nome_atributo_selecionado_02, populacao_carta01);
+                printf("2° Rodada: Valor do atributo %s para carta 2: %li \n", nome_atributo_selecionado_02, populacao_carta02);
+            break;
+            
+            case 2:
+                printf("2° Rodada: Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado_02, area_carta01);
+                printf("2° Rodada: Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado_02, area_carta02);
+            break;
+                        
+            case 3:
+                printf("2° Rodada: Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado_02, PIB_carta01);
+                printf("2° Rodada: Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado_02, PIB_carta01);
+            break;
         
-        if (carta_vencedora == CARTA_01) {
-            printf("A carta vencedora foi: %d\n", CARTA_01);
+            case 4:
+                printf("2° Rodada: Valor do atributo %s para carta 1: %d \n", nome_atributo_selecionado_02, numero_pontos_turisticos_carta01);
+                printf("2° Rodada: Valor do atributo %s para carta 2: %d \n", nome_atributo_selecionado_02, numero_pontos_turisticos_carta02);
+            break;
 
-        } else if (carta_vencedora == CARTA_02) {
-            printf("A carta vencedora foi: %d\n", CARTA_02);
+            case 5:
+                printf("2° Rodada: Valor do atributo %s para carta 1: %f \n", nome_atributo_selecionado_02, densidade_populacional_carta01);
+                printf("2° Rodada: Valor do atributo %s para carta 2: %f \n", nome_atributo_selecionado_02, densidade_populacional_carta02);
+            break;
 
-        } else if (carta_vencedora == EMPATE) {
-            printf("Empate!\n");
+            default:
+                break;
         }
+
+        printf("Soma dos atributos da CARTA 1: %f \n", soma_atributos_resultado_final_carta01);
+        printf("Soma dos atributos da CARTA 2: %f \n", soma_atributos_resultado_final_carta02);
+        
+        if (soma_atributos_resultado_final_carta01 > soma_atributos_resultado_final_carta02) {
+            printf("A CARTA 1 VENCEU! \n");
+            
+        } else if (soma_atributos_resultado_final_carta01 < soma_atributos_resultado_final_carta02) {
+            printf("A CARTA 2 VENCEU! \n");
+
+        } else if (soma_atributos_resultado_final_carta01 == soma_atributos_resultado_final_carta02) {
+            printf("AS CARTAS EMPATARAM! \n");
+        }
+
+        return 0;
     }
